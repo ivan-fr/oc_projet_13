cc6111e39ee117b25a2a8e6280309da2be9cb89c
besevic <ivan.besevic_fr@yahoo.com>
2019-05-24 22:19:18 +0200

    amélioration du script

--- oc_projet_13/settings/production.py
+++ oc_projet_13/settings/production.py
@@ -2,34 +2,6 @@ SECRET_KEY = '-~aO;| F;rE[??/w^zcumh(91'
 DEBUG = False
 ALLOWED_HOSTS = ['18.222.144.239']
 
-INSTALLED_APPS = [
-    'dal',
-    'dal_select2',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'treebeard',
-    'widget_tweaks',
-    'paypal.standard.ipn',
-    'swingtime',
-    'catalogue.apps.CatalogueConfig',
-    'ventes.apps.VentesConfig',
-    'session.apps.SessionConfig'
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
