332a0ad8f3802912e4cbb7b707c4285aa3538733
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2019-06-02 19:38:38 +0200

    Amélioration du websocket

--- oc_projet_13/settings/production.py
+++ oc_projet_13/settings/production.py
@@ -92,11 +92,14 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "oc_projet_13",
+        "USER": "ivan",
+        "PASSWORD": "hWfY7Uv82k7L9f2Sr._.",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
