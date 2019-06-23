fce09e526b4825dc2dbf33a4858f457866cae8f2
Ivan BESEVIC <ivan.besevic_fr@yahoo.com>
2019-06-23 12:09:37 +0200

    correction d'orthographe

---
+++ ventes/management/commands/d_activate.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from ventes.models import Commande
+
+
+class Command(BaseCommand):
+    help = 'Update the database for clean commands'
+
+    def handle(self, *args, **options):
+
+        with transaction.atomic():
+
+            commandes = Commande.objects.all()
+            commandes_counts = commandes.count()
+
+            self.stdout.write(self.style.SUCCESS(
+                f"Il y a {commandes_counts} commandes."
+            ))
+
+            i = 0
+            for commande in commandes:
+                if commande.enabled:
+                    commande.enabled = False
+                    commande.save()
+                    i += 1
+
+            self.stdout.write(self.style.SUCCESS(
+                f"Il y a {i} commande{'s' if i > 1 else ''} désactivé."
+            ))
